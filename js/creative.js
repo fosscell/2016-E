4e7be6262797bcdd48f40398e06c1e89e741e204
Shrimadhav U K <magicteacher95@gmail.com>
2016-02-03 23:29:47 +0530

    implement countdown timer api

--- js/creative.js
+++ js/creative.js
@@ -49,3 +49,5 @@ $(document).ready(function(){for(var j=1;j<=16;j++){var t1 = "qq" + j;document.g
 function backwards(e){return e.split("").reverse().join("")}function reveal(e,n,r,t){var a=backwards(e),d=backwards(t),c=d+a,i=document.createElement("a");i.innerHTML=a,i.href=c;var l=document.getElementById(n),m=document.getElementById(r);m.removeChild(l),m.appendChild(i)}
 
 $(window).ready(function(){$(".element").typed({strings:["Hack the Code"],typeSpeed:0,startDelay:200,showCursor:!0,cursorChar:"|"})});
+
+$(function(){$('.countdown').countdown({date: "Febraury 26, 2016 16:00:00",render: function(data){$(this.el).html("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span>, " + this.leadingZeros(data.hours, 2) + " <span>hrs</span>, " + this.leadingZeros(data.min, 2) + " <span>min</span>, " + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");}});});
