d5e02ac2a6858ec7f06ec509c989605483035e71
Shrimadhav U K <magicteacher95@gmail.com>
2016-02-06 20:42:19 +0530

    merge everything to one file!

--- js/schedule.js
+++ js/schedule.js
@@ -9,35 +9,6 @@ function resize()
 	var fs=(w*6)/w_hd+'em';
 	var magna=(w*4)/w_hd+'em';
 	var t_ar,t_sec;
-
-	/* scroll-wrap adjustment */
-	$("#scroll_wrap").css({'margin-top':7*h});
-	$(".wrap_inner>span").css({'top':(h-180)/2});	
-
-	/* scroll-wrap-inner adjustment */
-	$(".circle").css({'margin-top':(h-250)/2});
-
-	/* fix height adjustment */
-	$(".fix").css({'height':h});
-
-	/* section height adjustment */
-	$('section').each(function()
-	{
-		$(this).css({'height':h});
-	});	
-
-	/* window height adjustment */
-	$('.window').css({'height':(h/2)});
-
-	/* font adjustments */
-	$('body').css({'font-size':fs});
-
-	/* sec wrap adjustment */
-	$('.sec_wrap').each(function()
-	{
-		$(this).css({'margin-top':(h-$(this).height())/2});
-	});	
-
 }
 
 $(document).ready(function()
