183ffbf4ba1dee64ebbf05c25cf2b647fed887cc
Shrimadhav U K <magicteacher95@gmail.com>
2016-02-13 13:52:39 +0530

    modify color and fonts

--- js/functions.js
+++ js/functions.js
@@ -2,14 +2,13 @@
 /* Background Images
 -------------------------------------------------------------------*/
 var  pageTopImage = jQuery('#page-top').data('background-image');
-var  aboutImage = jQuery('#about').data('background-image');
-var  subscribeImage = jQuery('#subscribe').data('background-image');
-var  contactImage = jQuery('#contact').data('background-image');
 
 if (pageTopImage) {  jQuery('#page-top').css({ 'background-image':'url(' + pageTopImage + ')' }); };
-if (aboutImage) {  jQuery('#about').css({ 'background-image':'url(' + aboutImage + ')' }); };
-if (subscribeImage) {  jQuery('#subscribe').css({ 'background-image':'url(' + subscribeImage + ')' }); };
-if (contactImage) {  jQuery('#contact').css({ 'background-image':'url(' + contactImage + ')' }); };
+jQuery('#about').css({ 'background-color':'#3498db' });
+jQuery('#speakers').css({ 'background-color':'#fff' });
+jQuery('#schedule').css({ 'background-color':'#3498db' });
+jQuery('#contact').css({ 'background-color':'#fff' });
+jQuery('#reachus').css({ 'background-color':'#3498db' });
 
 /* Background Images End
 -------------------------------------------------------------------*/
