3f53c2e3b3336433ec26bd04812aa752ad7bd435
Shrimadhav U K <magicteacher95@gmail.com>
2016-02-20 11:56:56 +0530

    thank you @azharullah for fixing the error

--- admin/CMS/event_ticket.php
+++ admin/CMS/event_ticket.php
@@ -12,17 +12,36 @@ require_once('global.php');
 
 </head>
 <?php
-echo $_POST['parti_name'];
-echo $_POST['verifyusers'];
+$id = $_POST['parti_name'];
+$qry = "SELECT * FROM instamojo_responses WHERE ID = '$id' ;";
+$result = $mysqli->query($qry);
+if($result->num_row==1){
+$o_qry = "SELECT * FROM verifier ORDER BY FOSSMeet_ID DESC;";
+$o_result = $mysqli->query($o_qry);
+$req_num = dechex(($o_result->num_row) + 1);
+$row = $result->fetch_assoc();
+$id = $row['ID'];
+$name = $row['BUYER_NAME'];
+$fmid = $req_num;
+$n_qry = "INSERT INTO verifier VALUES('$fmid','$id');";
+if($mysqli->query($n_qry) === TRUE){
+        //echo "success";
+}
+else{
+        echo "you should not see this messgae!";
+        echo "fail";
+        die();
+}
+}
 ?>
-  <img src="img/blood.png" class="gwd-img-1azv">
+  <img src="img/logo.png" class="gwd-img-1azv">
     <img src="img/icfoss.png" class="gwd-img-1af3">
     <img src="img/sponsor.png" class="gwd-img-79cr">
     <p class="gwd-p-2290">FOSSMeet '16
     </p>
-    <p class="gwd-p-1rwh">ID&nbsp;</p>
+    <p class="gwd-p-1rwh"><?php echo $fmid ?>&nbsp;</p>
     <p class="gwd-p-ngqn">NIT,Calicut</p>
-    <p class="gwd-p-lfw1">name of participant</p>
+    <p class="gwd-p-lfw1"><?php echo $name ?></p>
 
 </body>
 </html>
